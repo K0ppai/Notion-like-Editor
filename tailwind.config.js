0539f703c80b89e6236ae0f7eb91e309ff8da86a
koppai <psthu.koppai@gmail.com>
2024-01-03 02:37:51 +0630

    Set up tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        gray: '#a3a8b3',
+        blue: '#3565a9',
+        light_green: '#d9f9e6',
+        dark_green: '#347659',
+        light_gray: '#e5e7eb',
+      },
+    },
+  },
+  plugins: [],
+};
