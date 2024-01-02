3c81b63a3b1b56f634c54af91e2e104669c41c72
koppai <psthu.koppai@gmail.com>
2024-01-03 05:55:28 +0630

    Update index.js for creating new headers

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const textInput = document.getElementById('textInput');
 const popUp = document.getElementById('popUp');
 const keyword = document.getElementById('keyword');
 const createHeaderBtn = document.getElementById('createHeaderBtn');
+const createExpandableBtn = document.getElementById('createExpandableBtn');
 
 const show = (element) => {
   element.classList.replace('hidden', 'block');
@@ -31,11 +32,71 @@ const handleInput = () => {
   }
 };
 
-textInput.addEventListener('input', handleInput);
-document.addEventListener('keydown', (e) => {
+const handleEscape = (e) => {
   if (e.key === 'Escape') {
-    hidePopup();
+    hide(popUp);
   }
-});
+};
 
+const deleteElement = (e) => {
+  const parent = e.target.closest('.elementContainer');
+  parent.remove();
+};
 
+const handleKeyDown = (e) => {
+  if (e.key === 'Enter') {
+    textInput.focus();
+  }
+};
+
+const createNewElement = (e) => {
+  const type = e.dataset.heading;
+  const element = document.createElement(type);
+
+  element.contentEditable = true;
+  element.dataset.placeholder = type === 'h1' ? 'Heading 1' : '';
+  element.classList = `${
+    type === 'h1'
+      ? 'text-lg font-bold ml-2 outline-none cursor-pointer'
+      : 'ml-2 outline-none cursor-pointer'
+  }`;
+
+  if (type === 'details') {
+    const summary = document.createElement('summary');
+    const p = document.createElement('p');
+    summary.contentEditable = true;
+    summary.dataset.placeholder = 'Expandable Header 1';
+    p.contentEditable = true;
+    p.dataset.placeholder = 'Paragraph';
+    summary.classList = 'font-bold';
+    element.append(summary, p);
+  }
+
+  return element;
+};
+
+const createNewDivElement = (e) => {
+  const button = e.currentTarget;
+
+  const div = document.createElement('div');
+  div.className = 'flex elementContainer my-3';
+
+  const template = document.getElementById('template');
+  const container = template.content.cloneNode(true);
+  const deleteBtn = container.querySelector('.deleteBtn');
+  const element = createNewElement(button);
+
+  element.addEventListener('keydown', handleKeyDown);
+  deleteBtn.addEventListener('click', deleteElement);
+
+  div.append(container, element);
+  outputContainer.appendChild(div);
+  hide(popUp);
+  textInput.value = '';
+  element.focus();
+};
+
+textInput.addEventListener('input', handleInput);
+document.addEventListener('keydown', handleEscape);
+createHeaderBtn.addEventListener('click', createNewDivElement);
+createExpandableBtn.addEventListener('click', createNewDivElement);
